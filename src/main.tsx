769b022364acc50d8f3054f463fa25a2b09883ae
Purnesh <purneshamzare@gmail.com>
2026-08-29 12:37:10 +0530

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createRouter, RouterProvider } from '@tanstack/react-router';
+
+import { routeTree } from './routeTree.gen';
+
+const router = createRouter({ routeTree, scrollRestoration: true });
+
+const queryClient = new QueryClient();
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router;
+  }
+}
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
